80bbebbca4c613f791f0f95f683e6892b5344a9b
Pavel Kochkin <kach93@mail.ru>
2023-11-15 11:22:20 +0100

    added edit and update events functonality

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,46 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { id } = useParams();
+
+  const { data, isPending } = useQuery({
+    queryKey: ["events", { id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const { mutate, isPending: isUpdatePending } = useMutation({
+    mutationFn: updateEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["events", { id }]);
+    },
+  });
+
+  console.log(data);
+
+  function handleSubmit(formData) {
+    mutate({ id, event: formData });
+
+    navigate("../");
+  }
 
   function handleClose() {
-    navigate('../');
+    navigate("../");
   }
+
+  if (isPending) return <LoadingIndicator />;
+  if (isUpdatePending) return <LoadingIndicator />;
 
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
